39e42d99be0fdb36dd33467b2faceb185432bce5
alexander <alexander.kanteruk@gmail.com>
2019-07-22 11:32:16 +0800

    2.5.3 owner

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0216/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task02.task0216;
+
+/* 
+Минимум трёх чисел
+*/
+public class Solution {
+    public static int min(int a, int b, int c) {
+        int min;
+        int d;
+        if (a < b)
+            d = a;
+        else
+            d = b;
+        if (c < d)
+            min = c;
+        else
+            min = d;
+        return min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
+}
