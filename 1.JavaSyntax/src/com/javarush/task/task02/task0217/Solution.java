6da79133b8f0e34055b0dad94f884548e49827d8
alexander <alexander.kanteruk@gmail.com>
2019-07-24 23:55:32 +0800

    3,01

--- 1.JavaSyntax/src/com/javarush/task/task02/task0217/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0217/Solution.java
@@ -4,11 +4,12 @@ package com.javarush.task.task02.task0217;
 Минимум четырех чисел
 */
 public class Solution {
+
     public static int min(int a, int b, int c, int d) {
         int min;
-        int z = min (a, b);
+        int z = min(a, b);
         int f;
-        if (c<z)
+        if (c < z)
             f = c;
         else
             f = z;
@@ -19,6 +20,7 @@ public class Solution {
         return min;
     }
 
+
     public static int min(int a, int b) {
         int min1;
         if (a < b)
@@ -28,6 +30,7 @@ public class Solution {
         return min1;
 
     }
+
 
     public static void main(String[] args) throws Exception {
         System.out.println(min(-20, -10));
