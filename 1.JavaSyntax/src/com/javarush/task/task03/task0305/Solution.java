54c3c66956c2324dd711e357b4f7c3dc77558322
alexander <alexander.kanteruk@gmail.com>
2019-07-25 14:48:36 +0800

    3,5

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0305;
+
+/* 
+Я так давно родился
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String month = "JUNE";
+        int age = 1990;
+        int day = 22;
+        System.out.print(month + " " + day + " " + age);
+
+
+    }
+}
